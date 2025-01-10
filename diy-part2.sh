48efe864006f83f441675d7815c7638f46c666c5
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-11 01:10:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,34 +11,34 @@
 #
 
 # 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
-sed -i 's/192.168.100.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-rm -rf ./feeds/extraipk/theme
+# sed -i 's/192.168.100.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# rm -rf ./feeds/extraipk/theme
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
-# sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By JayKwok'/g" package/base-files/files/etc/openwrt_release
-rm -rf feeds/extraipk/linkease/luci/luci-app-quickstart/htdocs/luci-static/quickstart/index.js
-cp -af istoreos/index.js feeds/extraipk/linkease/luci/luci-app-quickstart/htdocs/luci-static/quickstart/
-rm -rf package/base-files/files/etc/banner
-cp -af feeds/extraipk/patch/diy/banner  package/base-files/files/etc/banner
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-rm -rf feeds/extraipk/theme
+# rm -rf feeds/extraipk/linkease/luci/luci-app-quickstart/htdocs/luci-static/quickstart/index.js
+# cp -af istoreos/index.js feeds/extraipk/linkease/luci/luci-app-quickstart/htdocs/luci-static/quickstart/
+# rm -rf package/base-files/files/etc/banner
+# cp -af feeds/extraipk/patch/diy/banner  package/base-files/files/etc/banner
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+# rm -rf feeds/extraipk/theme
 
 ##更新FQ
-rm -rf feeds/packages/net/tailscale/*
-cp -af feeds/extraipk/tailscale/*  feeds/packages/net/tailscale/
-sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+# rm -rf feeds/packages/net/tailscale/*
+# cp -af feeds/extraipk/tailscale/*  feeds/packages/net/tailscale/
+# sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
 # ttyd 自动登录
 # sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
 
 ##MosDNS
-rm -rf feeds/packages/net/mosdns/*
-cp -af feeds/extraipk/op-mosdns/mosdns/* feeds/packages/net/mosdns/
-rm -rf feeds/packages/net/v2ray-geodata/*
-cp -af feeds/extraipk/op-mosdns/v2ray-geodata/* feeds/packages/net/v2ray-geodata/
+# rm -rf feeds/packages/net/mosdns/*
+# cp -af feeds/extraipk/op-mosdns/mosdns/* feeds/packages/net/mosdns/
+# rm -rf feeds/packages/net/v2ray-geodata/*
+# cp -af feeds/extraipk/op-mosdns/v2ray-geodata/* feeds/packages/net/v2ray-geodata/
 
-rm -rf feeds/luci/applications/luci-app-openclash/*
-cp -af feeds/extraipk/patch/wall-luci/luci-app-openclash/*  feeds/luci/applications/luci-app-openclash/
+# rm -rf feeds/luci/applications/luci-app-openclash/*
+# cp -af feeds/extraipk/patch/wall-luci/luci-app-openclash/*  feeds/luci/applications/luci-app-openclash/
 
 # 添加自定义软件包
 # echo '
