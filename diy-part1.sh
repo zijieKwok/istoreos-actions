8f1dc880e0ecb30591ce787c97cbbea3e0b155b5
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-11 01:01:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo -e "\nsrc-git extraipk https://github.com/zijieKwok/jaykwok-ipk" >> feeds.conf.default
-echo -e "\nsrc-git third https://github.com/jjm2473/openwrt-third.git;main" >> feeds.conf.default
+echo 'src-git extraipk https://github.com/zijieKwok/jaykwok-ipk' >> feeds.conf.default
+echo 'src-git third https://github.com/jjm2473/openwrt-third' >> feeds.conf.default
 # echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 # echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
 # echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
