6ca71506e9612facc63f733426b4e60d9d589ad8
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-12 13:25:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
-# sed -i 's/192.168.100.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.100.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # rm -rf ./feeds/extraipk/theme
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
@@ -20,13 +20,15 @@ sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By JayKwok'/g" package/base-
 # cp -af istoreos/index.js feeds/extraipk/linkease/luci/luci-app-quickstart/htdocs/luci-static/quickstart/
 # rm -rf package/base-files/files/etc/banner
 # cp -af feeds/extraipk/patch/diy/banner  package/base-files/files/etc/banner
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 # rm -rf feeds/extraipk/theme
 
 ##更新FQ
-# rm -rf feeds/packages/net/tailscale/*
-# cp -af feeds/extraipk/tailscale/*  feeds/packages/net/tailscale/
+rm -rf feeds/packages/net/tailscale/*
+git clone https://github.com/zijieKwok/jaykwok-ipk -b master/tailscale  feeds/packages/net/tailscale/
+touch feeds/luci/applications/luci-app-tailscale
+git clone https://github.com/zijieKwok/jaykwok-ipk -b master/luci-app-tailscale feeds/luc/applications/luci-app-tailscale
 # sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
 # ttyd 自动登录
 # sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
