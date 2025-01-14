72586c94a1d4fb898e224a07c79ac7450c6745ac
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-14 16:38:49 +0800

    Update op2.sh

--- diy/immortalwrt/op2.sh
+++ diy/immortalwrt/op2.sh
@@ -13,26 +13,26 @@ echo -e "msgstr \"控制\"" >> feeds/luci/modules/luci-base/po/zh_Hans/base.po
 
 
 ##配置IP
-sed -i 's/192\.168\.[0-9]*\.[0-9]*/192.168.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192\.168\.[0-9]*\.[0-9]*/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 ##
-rm -rf ./feeds/extraipk/theme/luci-theme-argon-18.06
-rm -rf ./feeds/extraipk/theme/luci-app-argon-config-18.06
-rm -rf ./feeds/extraipk/theme/luci-theme-design
-rm -rf ./feeds/extraipk/theme/luci-theme-edge
-rm -rf ./feeds/extraipk/theme/luci-theme-ifit
-rm -rf ./feeds/extraipk/theme/luci-theme-opentopd
-rm -rf ./feeds/extraipk/theme/luci-theme-neobird
-rm -rf ./feeds/extraipk/patch/luci-app-turboacc
+# rm -rf ./feeds/extraipk/theme/luci-theme-argon-18.06
+# rm -rf ./feeds/extraipk/theme/luci-app-argon-config-18.06
+# rm -rf ./feeds/extraipk/theme/luci-theme-design
+# rm -rf ./feeds/extraipk/theme/luci-theme-edge
+# rm -rf ./feeds/extraipk/theme/luci-theme-ifit
+# rm -rf ./feeds/extraipk/theme/luci-theme-opentopd
+# rm -rf ./feeds/extraipk/theme/luci-theme-neobird
+# rm -rf ./feeds/extraipk/patch/luci-app-turboacc
 
-rm -rf ./package/feeds/extraipk/luci-theme-argon-18.06
-rm -rf ./package/feeds/extraipk/luci-app-argon-config-18.06
-rm -rf ./package/feeds/extraipk/theme/luci-theme-design
-rm -rf ./package/feeds/extraipk/theme/luci-theme-edge
-rm -rf ./package/feeds/extraipk/theme/luci-theme-ifit
-rm -rf ./package/feeds/extraipk/theme/luci-theme-opentopd
-rm -rf ./package/feeds/extraipk/theme/luci-theme-neobird
-rm -rf ./package/feeds/extraipk/luci-app-turboacc
+# rm -rf ./package/feeds/extraipk/luci-theme-argon-18.06
+# rm -rf ./package/feeds/extraipk/luci-app-argon-config-18.06
+# rm -rf ./package/feeds/extraipk/theme/luci-theme-design
+# rm -rf ./package/feeds/extraipk/theme/luci-theme-edge
+# rm -rf ./package/feeds/extraipk/theme/luci-theme-ifit
+# rm -rf ./package/feeds/extraipk/theme/luci-theme-opentopd
+# rm -rf ./package/feeds/extraipk/theme/luci-theme-neobird
+# rm -rf ./package/feeds/extraipk/luci-app-turboacc
 
 
 ##取消bootstrap为默认主题
@@ -41,30 +41,30 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
 
 ##更改主机名
-sed -i "s/hostname='.*'/hostname='AE86Wrt'/g" package/base-files/files/bin/config_generate
+# sed -i "s/hostname='.*'/hostname='iStoreOS'/g" package/base-files/files/bin/config_generate
 
 ##加入作者信息
-sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='AE86Wrt-$(date +%Y%m%d)'/g"  package/base-files/files/etc/openwrt_release
-sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By DaoDao'/g" package/base-files/files/etc/openwrt_release
-cp -af feeds/extraipk/patch/diy/banner  package/base-files/files/etc/banner
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='iStoreOS-$(date +%Y%m%d)'/g"  package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By JayKwok'/g" package/base-files/files/etc/openwrt_release
+cp -af feeds/Jaykwok2999/patch/diy/banner  package/base-files/files/etc/banner
 
-sed -i "2iuci set istore.istore.channel='ae86_daodao'" package/emortal/default-settings/files/99-default-settings
+sed -i "2iuci set istore.istore.channel='istoreos_jaykwok'" package/emortal/default-settings/files/99-default-settings
 sed -i "3iuci commit istore" package/emortal/default-settings/files/99-default-settings
 
 ##WiFi
-# sed -i "s/ImmortalWrt/AE86/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i "s/OpenWrt/iStoreOS/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 
 ##MosDNS
-rm -rf feeds/packages/net/mosdns/*
-cp -af feeds/extraipk/op-mosdns/mosdns/* feeds/packages/net/mosdns/
+# rm -rf feeds/packages/net/mosdns/*
+# cp -af feeds/extraipk/op-mosdns/mosdns/* feeds/packages/net/mosdns/
 rm -rf feeds/packages/net/v2ray-geodata/*
 cp -af feeds/extraipk/op-mosdns/v2ray-geodata/* feeds/packages/net/v2ray-geodata/
 
 
 ##更新FQ
-rm -rf feeds/luci/applications/luci-app-passwall/*
-cp -af feeds/extraipk/patch/wall-luci/luci-app-passwall/*  feeds/luci/applications/luci-app-passwall/
+# rm -rf feeds/luci/applications/luci-app-passwall/*
+# cp -af feeds/extraipk/patch/wall-luci/luci-app-passwall/*  feeds/luci/applications/luci-app-passwall/
 
 # rm -rf feeds/luci/applications/luci-app-ssr-plus/*
 # cp -af feeds/extraipk/patch/wall-luci/luci-app-ssr-plus/*  feeds/luci/applications/luci-app-ssr-plus/
