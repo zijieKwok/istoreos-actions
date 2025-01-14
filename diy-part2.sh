cafef2e0db8fb659dd9c501d40b529144c91f98b
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-14 21:15:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,9 +14,9 @@
 sed -i 's/192.168.100.1/192.168.2.1/g' package/istoreos-files/Makefile
 
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By JayKwok'/g" package/base-files/files/etc/openwrt_release
-sed -i 's/fw0.koolcenter.com/ota.5588999.xyz/g' package/diy/luci-app-ota/root/bin/ota
+# sed -i 's/fw0.koolcenter.com/ota.5588999.xyz/g' package/diy/luci-app-ota/root/bin/ota
 # rm -rf feeds/Jaykwok2999/luci-app-bypass
 # rm -rf feeds/third_party/luci-app-LingTiGameAcc
 # rm -rf feeds/Jaykwok2999/luci-app-ssr-plus
