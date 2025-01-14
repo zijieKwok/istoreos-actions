126319335610ba5de8b84ead7775b3b061206ef0
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-14 11:52:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,9 +15,9 @@
 
 # Add a feed source
 echo 'src-git third_party https://github.com/linkease/istore-packages.git;main' >>feeds.conf.default
-echo 'src-git linkease_nas https://github.com/linkease/nas-packages.git;master' >>feeds.conf.default
-echo 'src-git Jaykwok2999 https://github.com/Jaykwok2999/istoreos-ipk.git;master' >> feeds.conf.default
-echo 'src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 echo 'src-git diskman https://github.com/jjm2473/luci-app-diskman.git;dev' >>feeds.conf.default
+echo 'src-git oaf https://github.com/jjm2473/OpenAppFilter.git;dev4' >>feeds.conf.default
+echo 'src-git linkease_nas https://github.com/linkease/nas-packages.git;master' >>feeds.conf.default
+echo 'src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci.git;main' >>feeds.conf.default
 echo 'src-git jjm2473_apps https://github.com/jjm2473/openwrt-apps.git;main' >>feeds.conf.default
-# echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+echo 'src-git Jaykwok2999 https://github.com/Jaykwok2999/istoreos-ipk.git;master' >>feeds.conf.default
