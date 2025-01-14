4510aab9acf66a9c5f2b5618db8b95b2980e113b
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-14 23:13:52 +0800

    Update op1.sh

--- diy/immortalwrt/op1.sh
+++ diy/immortalwrt/op1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #=================================================
-# DaoDao's script
+# JayKwok's script
 #=================================================
 ##添加自己的插件库
 echo -e "\nsrc-git Jaykwok2999 https://github.com/Jaykwok2999/istoreos-ipk" >> feeds.conf.default
