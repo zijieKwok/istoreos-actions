3ebb81601bbe4a8228a4094cec32e4ed0b2ebfe9
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-16 15:44:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # echo 'src-git third_party https://github.com/linkease/istore-packages.git;main' >>feeds.conf.default
