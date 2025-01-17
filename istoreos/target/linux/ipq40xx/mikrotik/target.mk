ecf20ee663ee37d27b1d8773d65efd1d84cabcaa
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-17 22:12:22 +0800

    Delete istoreos/target directory

--- istoreos/target/linux/ipq40xx/mikrotik/target.mk
+++
@@ -1,4 +0,0 @@
-BOARDNAME:=MikroTik
-FEATURES += minor nand
-KERNEL_IMAGES:=vmlinux
-IMAGES_DIR:=compressed
--- istoreos/target/linux/malta/le/target.mk
+++
@@ -1,9 +0,0 @@
-ARCH:=mipsel
-CPU_TYPE:=24kc
-SUBTARGET:=le
-BOARDNAME:=Little Endian
-
-define Target/Description
-	Build LE firmware images for MIPS Malta CoreLV board running in
-	little-endian mode
-endef
