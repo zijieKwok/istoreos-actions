437230ab2501872ef51c42a9c1c5741335067158
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-23 13:16:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,28 +11,28 @@
 #
 
 # 修改iStoreOS登陆ip,把下面的 192.168.100.1 修改成192.168.2.1
-# sed -i 's/192.168.100.1/192.168.2.1/g' package/istoreos-files/Makefile
+sed -i 's/192.168.100.1/192.168.2.1/g' package/istoreos-files/Makefile
 # rm -rf feeds/third_party/luci-app-LingTiGameAcc
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
 # sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By JayKwok'/g" package/base-files/files/etc/openwrt_release
-# sed -i 's/fw0.koolcenter.com/ota.5588999.xyz/g' package/diy/luci-app-ota/root/bin/ota
+sed -i 's/fw0.koolcenter.com/ota.5588999.xyz/g' package/diy/luci-app-ota/root/bin/ota
 # rm -rf feeds/Jaykwok2999/luci-app-bypass
 # rm -rf feeds/third_party/luci-app-LingTiGameAcc
 # rm -rf feeds/Jaykwok2999/luci-app-ssr-plus
 # rm -rf feeds/Jaykwok2999/luci-app-turboacc
 # rm -rf target/linux/x86/patches-6.6/*
 # cp -af $GITHUB_WORKSPACE/istoreos/patches-6.6/* target/linux/x86/patches-6.6/
-# rm -rf feeds/linkease_nas_luci/luci/luci-app-quickstart/htdocs/luci-static/quickstart/index.js
-# cp -af $GITHUB_WORKSPACE/istoreos/index.js feeds/linkease_nas_luci/luci/luci-app-quickstart/htdocs/luci-static/quickstart/
-# rm -rf package/base-files/files/etc/banner
-# cp -af $GITHUB_WORKSPACE/istoreos/banner package/base-files/files/etc/
-# rm -rf feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-# cp -af $GITHUB_WORKSPACE/istoreos/files/www/luci-static/argon/background/bg1.jpg feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/
-# rm -rf package/base-files/files/etc/passwd
-# cp -af $GITHUB_WORKSPACE/istoreos/passwd package/base-files/files/etc/
-# rm -rf package/base-files/files/etc/shadow
-# cp -af $GITHUB_WORKSPACE/istoreos/shadow package/base-files/files/etc/
+rm -rf feeds/linkease_nas_luci/luci/luci-app-quickstart/htdocs/luci-static/quickstart/index.js
+cp -af $GITHUB_WORKSPACE/istoreos/index.js feeds/linkease_nas_luci/luci/luci-app-quickstart/htdocs/luci-static/quickstart/
+rm -rf package/base-files/files/etc/banner
+cp -af $GITHUB_WORKSPACE/istoreos/banner package/base-files/files/etc/
+rm -rf feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -af $GITHUB_WORKSPACE/istoreos/files/www/luci-static/argon/background/bg1.jpg feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/
+rm -rf package/base-files/files/etc/passwd
+cp -af $GITHUB_WORKSPACE/istoreos/passwd package/base-files/files/etc/
+rm -rf package/base-files/files/etc/shadow
+cp -af $GITHUB_WORKSPACE/istoreos/shadow package/base-files/files/etc/
 # rm -rf feeds/packages/lang/golang/*
 # cp -af $GITHUB_WORKSPACE/istoreos/golang/* feeds/packages/lang/golang/
 
