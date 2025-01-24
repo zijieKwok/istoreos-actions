eff6e88445b2a56d7cba70a85c1d5f87d2dc9bba
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-24 12:22:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,20 +17,20 @@ sed -i 's/192.168.100.1/192.168.2.1/g' package/istoreos-files/Makefile
 # sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By JayKwok'/g" package/base-files/files/etc/openwrt_release
 rm -rf package/diy/luci-app-ota/root/bin/ota
-cp -af feeds/Jaykwok2999/patch/diy/ota package/diy/luci-app-ota/root/bin/
+cp -af feeds/istoreos_ipk/patch/diy/ota package/diy/luci-app-ota/root/bin/
 rm -rf package/base-files/files/etc/banner
-cp -af feeds/Jaykwok2999/patch/diy/banner package/base-files/files/etc/
+cp -af feeds/istoreos_ipk/patch/diy/banner package/base-files/files/etc/
 rm -rf feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-cp -af feeds/Jaykwok2999/patch/diy/bg1.jpg feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/
+cp -af feeds/istoreos_ipk/patch/diy/bg1.jpg feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/
 rm -rf package/base-files/files/etc/passwd
-cp -af feeds/Jaykwok2999/patch/diy/passwd package/base-files/files/etc/
+cp -af feeds/istoreos_ipk/patch/diy/passwd package/base-files/files/etc/
 rm -rf package/base-files/files/etc/shadow
-cp -af feeds/Jaykwok2999/patch/diy/shadow package/base-files/files/etc/
+cp -af feeds/istoreos_ipk/patch/diy/shadow package/base-files/files/etc/
 
 
 ##更新tailscale
 # rm -rf feeds/packages/net/tailscale/*
-# cp -af feeds/Jaykwok2999/tailscale/tailscale/*  feeds/packages/net/tailscale/
+# cp -af feeds/istoreos_ipk/tailscale/tailscale/*  feeds/packages/net/tailscale/
 # sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
 # ttyd 自动登录
 # sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
@@ -41,12 +41,12 @@ cp -af feeds/Jaykwok2999/patch/diy/shadow package/base-files/files/etc/
 
 ##MosDNS
 # rm -rf feeds/packages/net/mosdns/*
-# cp -af feeds/Jaykwok2999/op-mosdns/mosdns/* feeds/packages/net/mosdns/
-# rm -rf feeds/Jaykwok2999/net/v2ray-geodata/*
-# cp -af feeds/Jaykwok2999/op-mosdns/v2ray-geodata/* feeds/packages/net/v2ray-geodata/
+# cp -af feeds/istoreos_ipk/op-mosdns/mosdns/* feeds/packages/net/mosdns/
+# rm -rf feeds/istoreos_ipk/net/v2ray-geodata/*
+# cp -af feeds/istoreos_ipk/op-mosdns/v2ray-geodata/* feeds/packages/net/v2ray-geodata/
 
 # rm -rf feeds/luci/applications/luci-app-openclash/*
-# cp -af feeds/Jaykwok2999/patch/wall-luci/luci-app-openclash/*  feeds/luci/applications/luci-app-openclash/
+# cp -af feeds/istoreos_ipk/patch/wall-luci/luci-app-openclash/*  feeds/luci/applications/luci-app-openclash/
 
 # 添加自定义软件包
 # echo '
