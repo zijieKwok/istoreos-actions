59de613d308636b7f0c51795b732cf5816612e0a
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-24 10:14:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,17 +15,17 @@ sed -i 's/192.168.100.1/192.168.2.1/g' package/istoreos-files/Makefile
 # rm -rf feeds/third_party/luci-app-LingTiGameAcc
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
 # sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
-sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By JayKwok'/g" package/base-files/files/etc/openwrt_release
-rm -rf package/diy/luci-app-ota/root/bin/ota
-cp -af feeds/Jaykwok2999/patch/diy/ota package/diy/luci-app-ota/root/bin/
-rm -rf package/base-files/files/etc/banner
-cp -af feeds/Jaykwok2999/patch/diy/banner package/base-files/files/etc/
-rm -rf feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-cp -af feeds/Jaykwok2999/patch/diy/bg1.jpg feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/
-rm -rf package/base-files/files/etc/passwd
-cp -af feeds/Jaykwok2999/patch/diy/passwd package/base-files/files/etc/
-rm -rf package/base-files/files/etc/shadow
-cp -af feeds/Jaykwok2999/patch/diy/shadow package/base-files/files/etc/
+# sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By JayKwok'/g" package/base-files/files/etc/openwrt_release
+# rm -rf package/diy/luci-app-ota/root/bin/ota
+# cp -af feeds/Jaykwok2999/patch/diy/ota package/diy/luci-app-ota/root/bin/
+# rm -rf package/base-files/files/etc/banner
+# cp -af feeds/Jaykwok2999/patch/diy/banner package/base-files/files/etc/
+# rm -rf feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+# cp -af feeds/Jaykwok2999/patch/diy/bg1.jpg feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/
+# rm -rf package/base-files/files/etc/passwd
+# cp -af feeds/Jaykwok2999/patch/diy/passwd package/base-files/files/etc/
+# rm -rf package/base-files/files/etc/shadow
+# cp -af feeds/Jaykwok2999/patch/diy/shadow package/base-files/files/etc/
 
 
 ##更新tailscale
